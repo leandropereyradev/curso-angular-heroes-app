4943b7e824deaf4db1d5d72dc01177ec71b41d32
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-25 17:11:00 +0200

    HeroPageComponent

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'hero-heroes-page',
   templateUrl: './hero-page.component.html',
   styles: [],
 })
-export class HeroPageComponent {}
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHerById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        return (this.hero = hero);
+      });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
+}
