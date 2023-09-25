9de3b9746a3bb7a5b8bf27b3c506a3b845538a3a
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-25 12:56:10 +0200

    Auth and Heroes: Setting Routes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: 'list',
+        component: ListPageComponent,
+      },
+
+      // :id va al final porque sino los path anteriores caerían en este path
+      {
+        path: ':id',
+        component: HeroPageComponent,
+      },
+      // ----------------------------
+      {
+        path: '**',
+        redirectTo: 'list',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,24 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroesPageComponent } from './pages/heroes-page/heroes-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-
 @NgModule({
   declarations: [
-    HeroesPageComponent,
+    HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewHeroPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, HeroesRoutingModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
