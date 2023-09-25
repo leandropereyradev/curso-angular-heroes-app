2ec000154100e1c78e5c5581803590534cc51f38
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-25 15:41:43 +0200

    Card Hero Component

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewHeroPageComponent,
     SearchPageComponent,
+    CardComponent,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
