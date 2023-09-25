dbcbf8aa31f5dd0a9b9ade9277a5b03de51f7370
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-25 12:31:22 +0200

    Auth, Heroes, Material and Shared modules and components

---
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heroes-new-hero-page',
+  templateUrl: './new-hero-page.component.html',
+  styles: [],
+})
+export class NewHeroPageComponent {}
