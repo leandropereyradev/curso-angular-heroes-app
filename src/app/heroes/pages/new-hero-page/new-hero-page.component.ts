5f5b9a52fdc85641ca9e14f8a8322bc38dc454d7
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-27 11:33:30 +0200

    NewHeroComponent: Reactive Form: addHero()

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'heroes-new-hero-page',
   templateUrl: './new-hero-page.component.html',
   styles: [],
 })
-export class NewHeroPageComponent {
+export class NewHeroPageComponent implements OnInit {
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>(''),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     {
       id: 'DC Comics',
@@ -16,4 +31,33 @@ export class NewHeroPageComponent {
       desc: 'Marvel - Comics',
     },
   ];
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        //TODO mostrar snackbar
+      });
+
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero).subscribe({
+      // TODO mostrar snackbar y navegar a /heroes/edit/hero.is
+    });
+  }
 }
